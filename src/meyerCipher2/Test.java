c280f09d7ea713576b1c0cf7d5887217f809755a
Klemens Meyer <kmeyer@student.tgm.ac.at>
2018-10-12 14:11:48 +0200

    ShiftCipher-Class and Test-Class improved

--- src/meyerCipher2/Test.java
+++ src/meyerCipher2/Test.java
@@ -5,11 +5,29 @@ public class Test {
 		try {
 			SubstitutionCipher s1 = new SubstitutionCipher("qwertzuiopüyxcvbnmasdfghjklöäß");
 			
-			String text = s1.encrypt("Hello World! 123");
-			System.out.println(text);
-			System.out.println(s1.decrypt(text));
+			System.out.print("encrypt:\t");
+				String text = s1.encrypt("Hello World! 123");
+				System.out.println(text);
+			System.out.print("decrypt:\t");
+				System.out.println(s1.decrypt(text));
+			System.out.println("--------------------");
+			
 		}catch(ExceptionsMeyer e) {
 			System.out.println(e.toString());
+		}
+		
+		try {
+			ShiftCipher s1 = new ShiftCipher(3);
+			
+			System.out.print("encrypt:\t");
+				String text = s1.encrypt("Hello World! 123");
+				System.out.println(text);
+			System.out.print("decrypt:\t");
+				System.out.println(s1.decrypt(text));
+				System.out.println("--------------------");
+			
+		}catch(ExceptionsMeyer y) {
+			System.out.println(y.toString());
 		}
 	}
 }
